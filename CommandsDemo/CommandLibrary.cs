7288210dbaf6f71b928e2d1105ebcaa11bda5f2c
Kevin Hickey <kevin.hickey@aberdeen-asset.com>
2017-05-09 16:25:00 +1000

    Add project files.

---
+++ CommandsDemo/CommandLibrary.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Input;
+
+namespace CommandsDemo
+{
+    public static class CommandLibrary
+    {
+        private static RoutedUICommand add = new RoutedUICommand("Add", "Add", typeof(CommandLibrary));
+
+        private static RoutedUICommand remove = new RoutedUICommand("Remove", "Remove", typeof(CommandLibrary));
+
+        //static CommandLibrary()
+        //{
+        //    add = new RoutedUICommand("Add", "Add", typeof(CommandLibrary));
+        //}
+
+        public static RoutedUICommand AddListItem
+        {
+            get { return add; }
+        }
+
+        public static RoutedUICommand RemoveListItem
+        {
+            get { return remove; }
+        }
+
+    }
+}
---
+++ CommandsDemo/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CommandsDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnAddListItemExecute(object sender, ExecutedRoutedEventArgs e)
+        {
+            ListView lv = sender as ListView;
+            if (lv != null)
+            {
+                lv.Items.Add(e.Parameter);
+                textbox.Clear();
+                e.Handled = true;
+            }
+        }
+
+        private void OnRemoveListItemExecute(object sender, ExecutedRoutedEventArgs e)
+        {
+            ListView lv = sender as ListView;
+            if (lv != null)
+            {
+                if (lv.SelectedItem != null)
+                {
+                    lv.Items.Remove(lv.SelectedItem);
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void CanAddListItemExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = !String.IsNullOrEmpty(textbox.Text) && !listview.Items.Contains(textbox.Text);
+            e.Handled = true;
+        }
+
+        private void CanRemoveListItemExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            ListView lv = sender as ListView;
+            if (lv != null)
+            {
+                e.CanExecute = lv.SelectedItem != null;
+                e.Handled = true;
+            }
+        }
+
+    }
+}
